e884b8dc245dcca184c83f53414ba1ebffee3886
GrantvanB <gvanbo@gmail.com>
2021-07-02 10:49:36 -0600

    SignIn Form architecture & structure created

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './pages/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInUpPage from './pages/sign-in-up/sign-in-up.component';
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInUpPage} />
       </Switch>
     </div>
   );
