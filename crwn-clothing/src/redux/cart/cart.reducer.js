7487461644b700bdeeb36ba9d7927fc529d5f916
GrantvanB <gvanbo@gmail.com>
2021-07-05 16:03:09 -0600

    updated shop.data with unique id, and cart.reducer

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types'
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -6,7 +7,7 @@ const INITIAL_STATE = {
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-    switch (action.type) {
+    switch (action.type) { 
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
         default:
             return state;
