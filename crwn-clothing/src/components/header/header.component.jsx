c4d64c6f9f008175fabc57a2e1cb0bba40559fa3
GrantvanB <gvanbo@gmail.com>
2021-07-05 14:11:05 -0600

    cart button styled

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -9,7 +9,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss'
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo'/>
@@ -25,13 +25,15 @@ const Header = ({currentUser}) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
-
+        {
+            hidden ? null : <CartDropdown />
+        }
     </div>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
-})
+const mapStateToProps = ({user: {currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
+});
 
-export default connect(mapStateToProps )(Header)
+export default connect(mapStateToProps)(Header)
