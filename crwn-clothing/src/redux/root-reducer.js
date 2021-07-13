79a4e72ec37decc4c24bacf362bfab1f82be51f3
GrantvanB <gvanbo@gmail.com>
2021-07-13 11:26:43 -0600

    added storage with redux pesist

--- crwn-clothing/src/redux/root-reducer.js
+++ crwn-clothing/src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; //use local storage as default
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] // array of reducers that should be persist
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer
-});
+})
+
+export default persistReducer(persistConfig, rootReducer)
